8d9edec4ece941e283e5d80765dd9b96ce473ec3
Vaishnaviv114 <vaishnaviverma3452@gmail.com>
2026-06-21 21:08:29 +0530

    fix

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,8 +1,9 @@
 const aiService = require("../services/ai.service")
 
 
-module.exports.getResponse = async (req, res) => {
-    const prompt = req.query.prompt;
+module.exports.getReview = async (req, res) => {
+
+    const prompt = req.body.prompt;
 
     if(!prompt) {
         return res.status(400).send("Prompt is required");
