396c1e472b8b446dfdeaa5fc9c2dc04b9cdec1b1
Vaishnaviv114 <vaishnaviverma3452@gmail.com>
2026-07-13 14:49:45 +0530

    code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const aiRoutes = require("./routes/ai.routes")
-const cors = require('cors')
+const aiRoutes = require("./routes/ai.routes");
+const cors = require('cors');
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
-const reviewRoutes = require("./routes/review.routes");
 
-
-const app = express()
+const app = express();
 console.log("App Loaded");
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
+
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-app.use("/review", reviewRoutes);
+app.use("/ai", aiRoutes);
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
-app.use('/ai', aiRoutes)
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-module.exports = app 
+module.exports = app;
